2fe5ff85993a3b328b5095006259e6ce73f0a676
Alex <alexasensio04clase@gmail.com>
2025-11-21 11:08:24 +0100

    Añadido selector de imageens

--- app/src/main/java/es/ies/claudiomoyano/dam2/pmdm/practicaevaluable1evaluacion_asensio_sanchez_alex/EditarActivity.java
+++ app/src/main/java/es/ies/claudiomoyano/dam2/pmdm/practicaevaluable1evaluacion_asensio_sanchez_alex/EditarActivity.java
@@ -1,8 +1,12 @@
 package es.ies.claudiomoyano.dam2.pmdm.practicaevaluable1evaluacion_asensio_sanchez_alex;
 
+import android.Manifest;
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,14 +14,17 @@ import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class EditarActivity extends AppCompatActivity {
 
     int anioNacimineto, mesNacimiento, diaNacimiento, horaNacimiento, minutoNacimiento;
-
+    Uri imagenSeleccionada;
 
 
     @Override
@@ -71,8 +78,20 @@ public class EditarActivity extends AppCompatActivity {
         });
 
 
+        botonSeleccionaFoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (checkSelfPermission(Manifest.permission.READ_MEDIA_IMAGES) != PackageManager.PERMISSION_GRANTED) {
+                    requestPermissions(new String[]{Manifest.permission.READ_MEDIA_IMAGES}, 1);
+                } else {
+                    Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+                    intent.setType("image/*");
+                    intent.addCategory(Intent.CATEGORY_OPENABLE);
 
-
+                    startActivityForResult(intent, 2);
+                }
+            }
+        });
 
         botonAceptarEditar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -94,10 +113,42 @@ public class EditarActivity extends AppCompatActivity {
 
                 editor.putString("fechaNacimiento", diaNacimiento+"/"+mesNacimiento+"/"+anioNacimineto);
 
+                editor.putString("uriFotoSeleccionada", imagenSeleccionada.toString());
+
 
                 editor.apply();
 
             }
         });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] p, @NonNull int[] g) {
+        super.onRequestPermissionsResult(requestCode, p, g);
+
+        if (requestCode == 1) {
+            if (g.length > 0 && g[0] == PackageManager.PERMISSION_GRANTED) {
+                Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+                intent.setType("image/*");
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+
+                startActivityForResult(intent, 2);
+            } else {
+                Toast.makeText(this, "Permiso denegado", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 2 && resultCode == RESULT_OK && data != null) {
+            Uri uri = data.getData();
+
+            imagenSeleccionada = uri;
+        }
     }
 }
